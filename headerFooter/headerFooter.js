f0d45a08ab55ff7c618d2b08406e962ea116ce54
Xiriuu <joao_vf15@outlook.com>
2025-05-01 23:37:15 -0300

    Fix: Adicionada funcao JS para geracao do footer e header no index

--- headerFooter/headerFooter.js
+++ headerFooter/headerFooter.js
@@ -1,11 +1,52 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const pageTitle = document.getElementById("page-title");
+document.addEventListener("DOMContentLoaded", () => {
+    const loadFooter = async () => {
+        try {
+            const response = await fetch("./headerFooter/footer.html");
+            if (!response.ok) {
+                throw new Error(
+                    `Erro ao carregar o rodapé: ${response.statusText}`
+                );
+            }
+            const footerHTML = await response.text();
+            document.body.insertAdjacentHTML("beforeend", footerHTML);
+        } catch (error) {
+            console.error("Erro ao carregar o rodapé:", error);
+        }
+    };
 
-     // Carregar o rodapé
-    fetch("/assets/components/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML("beforeend", data);
-        });
-    
-})
+    loadFooter();
+
+    const loadHeader = async () => {
+        try {
+            const response = await fetch("./headerFooter/header.html");
+            if (!response.ok) {
+                throw new Error(
+                    `Erro ao carregar o cabeçalho: ${response.statusText}`
+                );
+            }
+            const headerHTML = await response.text();
+            document.body.insertAdjacentHTML("afterbegin", headerHTML);
+        } catch (error) {
+            console.error("Erro ao carregar o cabeçalho:", error);
+        }
+    };
+
+    loadHeader();
+
+    const linkHeaderCSS = () => {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "./headerFooter/header-footer.css";
+        document.head.appendChild(link);
+    };
+
+    linkHeaderCSS();
+});
+
+document.addEventListener("click", event => {
+    if (event.target.classList.contains("icon-menu")) {
+        event.target.classList.replace("icon-menu", "icon-cross");
+    } else if (event.target.classList.contains("icon-cross")) {
+        event.target.classList.replace("icon-cross", "icon-menu");
+    }
+});
